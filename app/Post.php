db0cdbb2d88c0674f3d035510d2ff69c1b7eada2
mdeias <mdeias@icloud.com>
2022-02-02 20:24:18 +0100

    tabella post con slug univoci

---
+++ app/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    public static function generateSlug($title){
+
+        //slug
+
+        $slug = Str::slug($title);
+        $slug_base = $slug;
+        // è presente nnel db?
+
+        $post_presente = Post::where('slug', $slug)->first();
+
+        // se è presente genero un contatore
+        $c = 1;
+        while($post_presente){
+            $slug = $slug_base . '-' . $c;
+            $c++;
+            $post_presente = Post::where('slug', $slug)->first();
+        }
+
+        return $slug;
+    }
+}
